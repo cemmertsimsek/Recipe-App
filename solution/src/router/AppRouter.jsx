c3ab2d375c3b0dfd9b1163431dceaa9584acf56a
cem <cemsimseksm@gmail.com>
2022-08-08 17:25:35 +0300

    private router

--- solution/src/router/AppRouter.jsx
+++ solution/src/router/AppRouter.jsx
@@ -4,6 +4,8 @@ import Navbar from '../components/nav/Navbar';
 import Home from "../pages/home/Home"
 import Login from "../pages/login/Login"
 import About from "../pages/about/About"
+import PrivateRouter from './PrivateRouter';
+import Detail from '../pages/detail/Detail';
 
 const AppRouter = () => {
   return (
@@ -13,7 +15,15 @@ const AppRouter = () => {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="login" element={<Login />} />
-            <Route path="about" element={<About />} />
+
+            <Route path="about" element={<PrivateRouter />}> 
+              <Route path="" element={<About />} />
+            </Route>
+            
+            <Route path="detail" element={<PrivateRouter />}> 
+              <Route path="" element={<Detail />} />
+            </Route>
+
         </Routes>
     </BrowserRouter>
   );
