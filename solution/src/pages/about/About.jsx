3a05bd79ea493dd2178a836997d7d24f06794385
cem <cemsimseksm@gmail.com>
2022-08-11 17:05:28 +0300

    about page updated

--- solution/src/pages/about/About.jsx
+++ solution/src/pages/about/About.jsx
@@ -8,19 +8,19 @@ const About = () => {
       <StyledImage src={codingSvg} />
       <div>
         <h1>
-          About Software Developer <span>Josh Smith</span>
+          About Software Developer <span>CEM MERT ŞİMŞEK</span>
         </h1>
       </div>
       <InfoContainer>
-        <h2>Hi, I'am Josh</h2>
+        <h2>Hi, I'am Cem</h2>
         <h3>I’m currently learning Full-Stack Development Languages.</h3>
         <h4>
-          I've already known JS, ReactJS, ReactNative, NodeJS, MongoDB,SQL,
-          Python, AWS Services.
+          I've already known JS, ReactJS, ReactNative, NodeJS, SQL,
+          Python.
         </h4>
         <h2>
-          <a href="mailto:josh.smith@clarusway.com">Send email</a> :
-          josh.smith@clarusway.com
+          <a href="mailto:josh.smith@example.com">Send email</a> :
+          josh.smith@example.com
         </h2>
       </InfoContainer>
     </AboutContainer>
